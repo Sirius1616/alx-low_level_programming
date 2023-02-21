c7c95a372b9231728f28c7578f82dc54b778c3f5
Sirius1616 <siriusa1.615@gmail.com>
2023-02-21 14:41:46 -0800

    Alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make apha ten times
+ * Return void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+	while (i > 10)
+	{
+		i = 'a';
+		while (i <= 'z')
+	}
+
+}
