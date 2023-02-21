1c62b0ea8646f23675a9ee7a0016ce3abe91117c
Sirius1616 <siriusa1.615@gmail.com>
2023-02-21 14:54:39 -0800

    ten alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,18 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
-	while (i > 10)
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
 	{
-		i = 'a';
-		while (i <= 'z')
+		ch = 'a';
+		while (ch <= 'z')
+		ch++;
 	}
+	_putchar('\n');
+	i++;
 
 }
