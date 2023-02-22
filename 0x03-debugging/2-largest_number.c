127c3fcfb7836d04392f9a9402375852d773cbe6
Sirius1616 <siriusa1.615@gmail.com>
2023-02-22 12:44:33 -0800

    Largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
@@ -20,13 +20,9 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if(c > b)
-	{
-		largest = c;
-	}
 	else
 	{
-		largest = b;
+		largest = c;
 	}
 
 	return (largest);
