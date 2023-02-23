a3111ccd55e891ee238f120380ef8071e9d85515
Sirius1616 <siriusa1.615@gmail.com>
2023-02-23 08:31:04 -0800

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ * _isdigit - a function that checks for a digit
+ * @c: the argument to be checked
+ * Return: 0 always when finished
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0, &&c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
