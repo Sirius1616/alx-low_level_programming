4fadc8bebecfbcae6f0b030b78dc10221972be43
Sirius1616 <siriusa1.615@gmail.com>
2023-02-27 11:16:08 -0800

    98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that sends a pointer to 98
+ * @n: The pointer that points to 98
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Solution to project on C pointer, array and strings
