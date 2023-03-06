2a6f93176adb9b3b6a256cf8cf4b24c643a809a4
Sirius1616 <siriusa1.615@gmail.com>
2023-03-06 12:35:51 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - a function that returns a part of string in another string
+ * @s: the main string
+ * @accept: the sub_string
+ * Return: always s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s);
+			}
+		}
+	}
+	return ('\0');
+}
