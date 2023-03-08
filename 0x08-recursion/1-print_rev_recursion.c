ffaae0f47c6a85fa21b27099bcb4ab9161744da3
Sirius1616 <siriusa1.615@gmail.com>
2023-03-08 11:37:57 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _print_rev_recursion - a function that recursively prints out a string
- * @str: the string to be printed out recursively
+ * @s: the string to be printed out recursively
  *
  */
 
