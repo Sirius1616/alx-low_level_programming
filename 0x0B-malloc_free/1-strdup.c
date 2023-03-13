ea31eb8aa83efcefe62fb66bc0e79e7ecf9be5ac
Sirius1616 <siriusa1.615@gmail.com>
2023-03-13 14:32:58 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,20 +12,20 @@
 char *_strdup(char *str)
 {
 	char *str0;
-	int i = 0;
-	int j;
+	unsigned int i = 0;
+	unsigned int j;
 
 	if (str == NULL)
 		return (NULL);
 	while (str[i] != '\0')
 		i++;
-	str0 = malloc(sizeof(char) * (i + 1));
+	str0 = malloc((sizeof(str0) * i) + 1);
 
 	if (str0 == NULL)
 		return (NULL);
 
 	for (j = 0; str0[j]; j++)
-		str0[j] = str[i];
+		str0[j] = str[j];
 	return (str0);
 }
 
