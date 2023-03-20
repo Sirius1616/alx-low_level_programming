e2106b2e1eb6d087c8e9b8b2944cb16724cf4e89
Sirius1616 <siriusa1.615@gmail.com>
2023-03-20 09:15:08 -0700

    a new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - a structure that holds several items for dog
- * @name: first member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
- */
+* struct dog - a dog's basic info
+* @name: first attribute
+* @age: second attribute
+* @owner: third attribute
+*/
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+
+#endif
