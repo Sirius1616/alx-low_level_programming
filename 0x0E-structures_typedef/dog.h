1ba15ff125503c87d8c3563b242a5f9c0aae8971
Sirius1616 <siriusa1.615@gmail.com>
2023-03-20 09:03:17 -0700

    a new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog{
+/**
+ * struct dog - a structure that holds several items for dog
+ * @name: first member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
 	char* name;
 	float age;
 	char* owner;
 };
+
