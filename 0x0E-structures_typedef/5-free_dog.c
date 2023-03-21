711de971b9ac7ba3e290e73b635d89926d698c46
Sirius1616 <siriusa1.615@gmail.com>
2023-03-21 13:55:22 -0700

    a function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs
+ * dog_t: the data type for the dog
+ * d: the dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+
+}
