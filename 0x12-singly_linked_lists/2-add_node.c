34e73375a31eb5ebc56fa6b17e9f5df049f8f2c2
Sirius1616 <siriusa1.615@gmail.com>
2023-03-30 11:31:13 -0700

    a function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
-	return (NULL);
+		return (NULL);
 
 	temp->str = strdup(str);
 	if (temp->str == NULL)
@@ -23,8 +23,26 @@ list_t *add_node(list_t **head, const char *str)
 		free(temp);
 		return (NULL);
 	}
-
+	
+	temp->len = _strlen(str);
 	temp->next = *head;
 	*head = temp;
 	return (*head);
+}
+
+/**
+ * _strlen - function to calculate the length of a const char string
+ * @s: string to work with
+ * Return: length of string.
+ */
+
+
+int _strlen(const char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
