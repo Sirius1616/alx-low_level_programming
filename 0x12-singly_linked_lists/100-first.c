3f099b1aa603b9aeaf9eb1db34dad832b3d03321
Sirius1616 <siriusa1.615@gmail.com>
2023-03-31 13:46:27 -0700

    a function that executes before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
- * siriusa - a function that prints a sentence
- *
+ * first - prints a sentence before the main
+ * function is executed
  */
-
-
-void siriusa(void) __attribute__ ((constructor));
-
-void siriusa(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
