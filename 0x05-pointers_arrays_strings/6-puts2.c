9513e4b17c7df1ed7ad3810b08a1e23f6960ca66
Sirius1616 <siriusa1.615@gmail.com>
2023-05-05 14:53:15 -0700

    correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,25 +7,13 @@
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
-
-	while (*y != '\0')
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		y++;
-		longi++;
+		_putchar(str[i]);
+		i++;
 	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
-	}
-	_putchar('\n');
 }
 
 
