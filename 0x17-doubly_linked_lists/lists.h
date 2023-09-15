287507aea9d876cd492807fc08ff5b378c39379e
Sirius1616 <siriusa1.615@gmail.com>
2023-09-15 21:43:56 +0000

    new

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef "LISTS_H"
-#define "LISTS_H"
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -11,14 +13,12 @@
  * Description: doubly linked list node structure
  * for Holberton project
  */
-
-struct node{
-		struct node *prev;
-		struct node *next;
-		int data;
-};
-
-typedef struct node dlistint_t;
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -30,4 +30,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif
+#endif /* LISTS_H */
